653e57929191fd7a5cd4d660221d84f2896798e5
lukasz.pulawski <lukasz.pulawski@payu.com>
2014-10-14 17:00:28 +0200

    A complete paymetn cycle in Test Bean.

--- client/src/main/java/com/payu/soa/example/client/TestBean.java
+++ client/src/main/java/com/payu/soa/example/client/TestBean.java
@@ -1,8 +1,9 @@
 package com.payu.soa.example.client;
 
-
 import java.math.BigDecimal;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.remoting.RemoteConnectFailureException;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -22,6 +23,8 @@ import com.payu.user.server.service.UserService;
 
 @Component
 public class TestBean {
+
+	private static Log LOG = LogFactory.getLog(TestBean.class);
 
 	@Discover
 	@RetryPolicy(exception = RemoteConnectFailureException.class)
@@ -45,55 +48,91 @@ public class TestBean {
 		pay(sessionId, paymentMethodBrand, posId);
 	}
 
-
 	private void pay(int sessionId, String paymentMethodBrand, Long posId) {
 
 		verifyPos(sessionId, paymentMethodBrand, posId);
-		
+
 		Long userId = createAndActivateUser();
-		
+
 		Order order = createOrder(sessionId, userId);
 		Long orderId = orderService.createOrder(order);
 		BigDecimal amount = order.getAmount();
 
-		
 		Long transId = createTransaction(paymentMethodBrand, orderId, amount);
 		
-		verifyPaymentProcess(transId, userId, orderId, sessionId, amount, paymentMethodBrand, posId);
+		LOG.info("*** Transaction created  ***");
+
+		verifyPaymentProcess(transId, userId, orderId, sessionId, amount,
+				paymentMethodBrand, posId);
+		
+		LOG.info("*** Transaction verified ***");
 	}
 
+	private boolean verifyPaymentProcess(Long transId, Long userId,
+			Long orderId, int sessionId, BigDecimal amount,
+			String paymentMethodBrand, Long posId) {
 
-	private void verifyPaymentProcess(Long transId, Long userId, Long orderId,
-			int sessionId, BigDecimal amount, String paymentMethodBrand,
-			Long posId) {
-		
-		Order order = orderService.getOrder(orderId);
-		
-//		assertThat(order.getId()).isEqualTo(orderId);
-//		assertThat(order.getUserId()).isEqualTo(userId);
-//		assertThat(order.getAmount()).isEqualTo(amount);
-		
-		
-		
+		return verifyUser(userId)// 
+				&& verifyOrder(userId, orderId)
+				&& verifyTransaction(transId, orderId);
+
+	}
+
+	private boolean verifyTransaction(Long transId, Long orderId) {
 		Transaction trans = transService.getTransactionById(transId);
-		
-//		assertThat(trans.getOrderId()).isEqualTo(orderId);
-//		assertThat(trans.getAmount()).isEqualTo(amount);
-		
-		
+
+		if (trans == null) {
+			LOG.error("Transaction not created");
+			return false;
+		}
+
+		if (!orderId.equals(trans.getOrderId())) {
+			LOG.error("Order not bound to transaction ");
+			return false;
+		}
+		return true;
 	}
 
+	private boolean verifyOrder(Long userId, Long orderId) {
+		Order order = orderService.getOrder(orderId);
+
+		if (order == null) {
+			LOG.error("Order not created");
+			return false;
+		}
+
+		if (!userId.equals(order.getUserId())) {
+			LOG.error("Order not created");
+			return false;
+		}
+		return true;
+	}
+
+	private boolean verifyUser(Long userId) {
+		User user = userService.getUserById(userId);
+
+		if (user == null) {
+			LOG.error("User not created");
+			return false;
+		}
+
+		if (!user.isActive()) {
+			LOG.error("User not activaated");
+			return false;
+		}
+		return true;
+	}
 
 	private Long createTransaction(String paymentMethodBrand, Long orderId,
 			BigDecimal amount) {
-		Transaction trans  = new Transaction(amount, paymentMethodBrand, orderId); 
+		Transaction trans = new Transaction(amount, paymentMethodBrand, orderId);
 		Long transId = transService.createTransaction(trans);
 		return transId;
 	}
 
-
 	private Long createAndActivateUser() {
-		Long userId = userService.createUser(new User("Jan Kowalski", "jan.kowalski@gmail.com", "74030200430"));
+		Long userId = userService.createUser(new User("Jan Kowalski",
+				"jan.kowalski@gmail.com", "74030200430"));
 		try {
 			userService.activateUser(userId);
 		} catch (NoSuchUserException e) {
@@ -102,20 +141,21 @@ public class TestBean {
 		return userId;
 	}
 
-
 	private void verifyPos(int sessionId, String paymentMethodBrand, Long posId) {
 		Pos pos = posService.getPosById(posId);
-		if (!pos.isActive() || !pos.isPaymetnMethodBrandActive(paymentMethodBrand)) {
-			throw new IllegalStateException(String.format("Payment for % not possible with method %s and pos %s ", sessionId, paymentMethodBrand, pos.getName()));
+		if (!pos.isActive()
+				|| !pos.isPaymetnMethodBrandActive(paymentMethodBrand)) {
+			throw new IllegalStateException(String.format(
+					"Payment for % not possible with method %s and pos %s ",
+					sessionId, paymentMethodBrand, pos.getName()));
 		}
 	}
-	
-	
-    private Order createOrder(int sessionId, Long userId) {
-        Order order = new Order(new BigDecimal(Math.random() * 10000L), "Foo " + sessionId);
-        order.setUserId(userId);
-		return order;
-    }
 
+	private Order createOrder(int sessionId, Long userId) {
+		Order order = new Order(new BigDecimal(Math.random() * 10000L), "Foo "
+				+ sessionId);
+		order.setUserId(userId);
+		return order;
+	}
 
 }
